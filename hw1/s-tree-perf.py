69df179a719726c32a4c6617f5fc94bcaaba2854
RinHizakura <s921975628@gmail.com>
2023-08-06 23:13:30 +0800

    Introduce benchmarking script and interface

---
+++ hw1/s-tree-perf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def bench(algo, n, pat):
+    binary = 'treeint.out'
+
+    times = os.popen(f"taskset -c 1 ./{binary} {algo} {n}").read()
+    # strip out the '\n'
+    times = times.strip()
+    times = [int(x) for x in times.split(',')]
+
+    return times[pat]
+
+# make sure we do make before everything start
+os.system("make")
+
+# 0: insert, 1: find, 2: remove
+pat = 0
+
+algo_list=["baseline"]
+nsize = list(int(2**k) for k in range(7, 20))
+ts = [[bench(algo, size, pat) for size in nsize] for algo in algo_list]
+
+fig, ax = plt.subplots()
+for idx, t in enumerate(ts):
+    ax.plot(nsize, t, label=algo_list[idx])
+ax.set_xlabel('Tree size')
+ax.set_xscale('log', base=2)
+ax.set_ylabel('Latency(ns)')
+ax.set_ylim(bottom=0, top=None)
+ax.legend()
+plt.show()
--- hw1/treeint.c
+++ hw1/treeint.c
@@ -2,13 +2,22 @@
 #include <stddef.h> /* offsetof */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "s_tree.h"
+
+//#define PRINT_DEBUG
 
 #define container_of(ptr, type, member) \
     ((type *) ((char *) (ptr) - (offsetof(type, member))))
 
 #define treeint_entry(ptr) container_of(ptr, struct treeint, st_n)
+
+#ifdef PRINT_DEBUG
+#define pr_debug(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define pr_debug(...)
+#endif
 
 struct treeint {
     int value;
@@ -61,12 +70,18 @@ int treeint_insert(int a)
     return st_insert(tree, (void *) &a);
 }
 
+struct st_node *treeint_find(int a)
+{
+    return st_find(tree, (void *) &a);
+}
+
 int treeint_remove(int a)
 {
     return st_remove(tree, (void *) &a);
 }
 
 /* ascending order */
+#ifdef PRINT_DEBUG
 static void __treeint_dump(struct st_node *n, int depth)
 {
     if (!n)
@@ -75,42 +90,86 @@ static void __treeint_dump(struct st_node *n, int depth)
     __treeint_dump(st_left(n), depth + 1);  // FFFF
 
     struct treeint *v = treeint_entry(n);
-    printf("%d\n", v->value);
+    pr_debug("%d\n", v->value);
 
     __treeint_dump(st_right(n), depth + 1);  // GGGG
 }
+#else
+static void __treeint_dump(__attribute__((unused)) struct st_node *n,
+                           __attribute__((unused)) int depth)
+{
+    // do nothing
+}
+#endif
 
 void treeint_dump()
 {
     __treeint_dump(st_root(tree), 0);
 }
 
-int main()
+#define rand_key(sz) rand() % ((sz) -1)
+
+/* Just a naive implementation to benchmark a code block. */
+#define bench(statement)                                                  \
+    ({                                                                    \
+        struct timespec _tt1, _tt2;                                       \
+        clock_gettime(CLOCK_MONOTONIC, &_tt1);                            \
+        statement clock_gettime(CLOCK_MONOTONIC, &_tt2);                  \
+        long long time = (long long) (_tt2.tv_sec * 1e9 + _tt2.tv_nsec) - \
+                         (long long) (_tt1.tv_sec * 1e9 + _tt1.tv_nsec);  \
+        time;                                                             \
+    })
+
+int main(int argc, char *argv[])
 {
+    if (argc < 3) {
+        printf("usage: treeint <algo> <tree size>\n");
+        return -1;
+    }
+
+    if (strcmp(argv[1], "baseline") != 0) {
+        // TODO: Let's support algorithm other than baseline?
+        printf("Invalid algorithm %s\n", argv[1]);
+        return -3;
+    }
+
+    size_t tree_size = 0;
+    if (!sscanf(argv[2], "%ld", &tree_size)) {
+        printf("Invalid tree size %s\n", argv[2]);
+        return -3;
+    }
+
     srand(time(0));
 
     treeint_init();
 
-    for (int i = 0; i < 100; ++i)
-        treeint_insert(rand() % 99);
+    long long insert_time = bench(for (size_t i = 0; i < tree_size; ++i)
+                                      treeint_insert(rand_key(tree_size)););
 
-    printf("[ After insertions ]\n");
+    pr_debug("[ After insertions ]\n");
     treeint_dump();
 
-    printf("Removing...\n");
-    for (int i = 0; i < 100; ++i) {
-        int v = rand() % 99;
-        printf("%2d  ", v);
-        if ((i + 1) % 10 == 0)
-            printf("\n");
-        treeint_remove(v);
-    }
-    printf("\n");
+    long long find_time = bench(for (size_t i = 0; i < tree_size; ++i)
+                                    treeint_find(rand_key(tree_size)););
 
-    printf("[ After removals ]\n");
+    pr_debug("Removing...\n");
+    long long remove_time = bench(for (size_t i = 0; i < tree_size; ++i) {
+        int v = rand_key(tree_size);
+        pr_debug("%2d  ", v);
+        if ((i + 1) % 10 == 0) {
+            pr_debug("\n");
+        }
+        treeint_remove(v);
+    });
+    pr_debug("\n");
+
+    pr_debug("[ After removals ]\n");
     treeint_dump();
 
     treeint_destroy();
+
+    printf("%lld, %lld, %lld\n", insert_time / tree_size, find_time / tree_size,
+           remove_time / tree_size);
 
     return 0;
 }
